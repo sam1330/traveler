36e0035c46b56ba075e279c34b5d9e669152e0f4
sam1330 <sammy1301m@gmail.com>
2022-02-17 08:58:40 -0400

    created and imported components to use

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { CssBaseline, Grid } from '@material-ui/core';
+
+// COMPONENTS
+import Header from './components/Header/Header';
+import List from './components/List/List';
+import Map from './components/Map/Map';
+import PlaceDetails from './components/PlaceDetails/PlaceDetails';
+
+
+const App = () => {
+
+	return (
+		<>
+			<CssBaseline />
+			<Header />
+		</>
+	)
+};
+
+export default App;
